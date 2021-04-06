0e4124576fa1045c057d7913df98eca6256dfa8d
anaisrdn <63746533+anaisrdn@users.noreply.github.com>
2021-04-07 00:41:07 +0200

    Add files via upload

--- php/connexion.php
+++ php/connexion.php
@@ -1,4 +1,42 @@
-<?php session_start(); ?>
+<?php 
+session_start();
+if(isset($_POST['motdepasse']) and !empty($_POST['motdepasse']) and isset($_POST['identifiant']) and !empty($_POST['identifiant'])){
+	$mdp=$_POST['motdepasse'];
+	$idf=$_POST['identifiant'];
+$Fichier = "connexion.txt";
+if (is_file($Fichier)) {
+	$verification=FALSE;
+	$comp= $idf.';'.$mdp;
+	if ($TabFich =file($Fichier)) {
+		for($i = 0; $i <count($TabFich);$i++){ 
+		if($comp==$TabFich[$i]){
+			$verification=TRUE;
+		}
+		
+		if($verification=TRUE){
+		$_SESSION['identifiant']=$idf;
+		$_SESSION['motdepasse']=$mdp;
+		header ("location: ../index.php?id=".$_SESSION['identifiant']);
+			}
+			else{
+				$msg="Mauvais identifiant ou mot de passe !";
+				
+				}
+		
+		
+		
+		}
+			}
+		else {
+			 $msg="Le fichier ne peut être lu...<br>"; } }
+			else {
+			 $msg="Désolé le fichier n'est pas valide<br> "  ; }
+				
+
+
+}
+
+ ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +54,7 @@
 
 <section id="milieu">
     <h1>Connexion</h1>
-    <form id="form_contact">
+    <form id="form_contact"  method="post" action="">
         <table>
             <tr>
                 <td> <label for="identifiant">Identifiant : </label> </td>
@@ -28,8 +66,13 @@
             </tr>
             <tr><td><input type="submit" value="Se connecter" id="bouton_connexion"></td></tr>
     </table>
-        
+        <a href="./deconnexion.php">Se deconnecter</a>
     </form>
+    
+    <?php
+if(isset($msg)){
+	echo '<h2 style="color:#ED2F2F">'.$msg.'</h2>'; 
+} ?>
 </section>
 
 <?php include './footer.php'; ?>
--- php/header_index.php
+++ php/header_index.php
@@ -1,21 +1,13 @@
-<?php include './php/varSession.inc.php' ;?>
 <header> 
 <nav id="top">
 <img src="./img/logo.png" alt="logo QualiTea">
 <ul id="ul_top">
-    <li><a href="./index.php">Accueil</href></a></li>
-    <?php 
-    $taille=count($_SESSION['categorie']);
-
-    foreach ($_SESSION['categorie'] as $x){
-        $y=$x[5];
-        $z=$x[6];
-        echo "<li><a href=\"./php/produits.php?categorie=$y\">$z</href></a></li>";
-    }
-
-    ?>
-    <li><a href="./php/contact.php" >Contact</href></a></li>
-    <li><a href="./php/connexion.php">Connexion</href></a></li>
+    <li><a href="./index.php?id=<?php echo $_SESSION['identifiant']?>">Accueil</href></a></li>
+    <li><a href="./php/produits.php?categorie=the_vert?id=<?php echo $_SESSION['identifiant']?>">Thés verts</href></a></li>
+    <li><a href="./php/produits.php?categorie=the_noir?id=<?php echo $_SESSION['identifiant']?>">Thés noirs</href></a></li>
+    <li><a href="./php/produits.php?categorie=infusion?id=<?php echo $_SESSION['identifiant']?>">Infusions</href></a></li>
+    <li><a href="./php/contact.php?id=<?php echo $_SESSION['identifiant']?>">Contact</href></a></li>
+    <li><a href="./php/connexion.php?id=<?php echo $_SESSION['identifiant']?>">Connexion</href></a></li>
 </ul>
 </nav>
 </header>
